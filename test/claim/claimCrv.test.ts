b6f772599d778c50b83e380fd3ea004d9cf9562e
James <james@hummingbot.io>
2022-05-13 14:55:01 +0200

    Apply prettier, routerExchange returns a transaction, routerExchange accepts more options

--- test/claim/claimCrv.test.ts
+++ test/claim/claimCrv.test.ts
@@ -1,5 +1,5 @@
-import { assert } from "chai";
-import curve from "../../src/";
+import { assert } from 'chai';
+import curve from '../../src/';
 
 // ----------------
 // TO MAKE THIS TEST WORKING YOU NEED TO DO THESE STEPS FIRST
@@ -13,37 +13,91 @@ import curve from "../../src/";
 //
 // ----------------
 
-
-const PLAIN_POOLS =  ['susd', 'ren', 'sbtc', 'hbtc', '3pool', 'seth', 'steth', 'ankreth', 'link', 'reth', 'eurt']; // Without eurs
-const LENDING_POOLS = ['compound', 'usdt', 'y', 'busd', 'pax', 'aave', 'saave', 'ib'];
-const META_POOLS = ['gusd', 'husd', 'usdk', 'usdn', 'musd', 'rsv', 'tbtc', 'dusd', 'pbtc', 'bbtc', 'obtc', 'ust', 'usdp', 'tusd', 'frax', 'lusd', 'busdv2', 'alusd', 'mim'];
+const PLAIN_POOLS = [
+  'susd',
+  'ren',
+  'sbtc',
+  'hbtc',
+  '3pool',
+  'seth',
+  'steth',
+  'ankreth',
+  'link',
+  'reth',
+  'eurt',
+]; // Without eurs
+const LENDING_POOLS = [
+  'compound',
+  'usdt',
+  'y',
+  'busd',
+  'pax',
+  'aave',
+  'saave',
+  'ib',
+];
+const META_POOLS = [
+  'gusd',
+  'husd',
+  'usdk',
+  'usdn',
+  'musd',
+  'rsv',
+  'tbtc',
+  'dusd',
+  'pbtc',
+  'bbtc',
+  'obtc',
+  'ust',
+  'usdp',
+  'tusd',
+  'frax',
+  'lusd',
+  'busdv2',
+  'alusd',
+  'mim',
+];
 const CRYPTO_POOLS = ['tricrypto2', 'eurtusd', 'crveth', 'cvxeth'];
-const ETHEREUM_POOLS = [...PLAIN_POOLS, ...LENDING_POOLS, ...META_POOLS, ...CRYPTO_POOLS];
+const ETHEREUM_POOLS = [
+  ...PLAIN_POOLS,
+  ...LENDING_POOLS,
+  ...META_POOLS,
+  ...CRYPTO_POOLS,
+];
 
 const POLYGON_POOLS = ['aave', 'ren', 'atricrypto3', 'eurtusd'];
 
+describe('Claiming CRV', function () {
+  this.timeout(120000);
 
-describe('Claiming CRV', function() {
-    this.timeout(120000);
+  before(async function () {
+    await curve.init('JsonRpc', {}, { gasPrice: 0 });
+  });
 
-    before(async function() {
-        await curve.init('JsonRpc', {}, { gasPrice: 0 });
+  for (const poolName of CRYPTO_POOLS) {
+    it(`Claims CRV from ${poolName.toUpperCase()}`, async function () {
+      const pool = new curve.Pool(poolName);
+
+      const [crvBalanceBefore] = (await curve.getBalances(['crv'])) as string[];
+      const expected = await pool.gaugeClaimableTokens();
+
+      console.log(
+        crvBalanceBefore,
+        '+',
+        expected,
+        '=',
+        Number(crvBalanceBefore) + Number(expected)
+      );
+      await pool.gaugeClaimTokens();
+
+      const [crvBalanceAfter] = (await curve.getBalances(['crv'])) as string[];
+      console.log(crvBalanceAfter);
+
+      assert.approximately(
+        Number(crvBalanceAfter),
+        Number(crvBalanceBefore) + Number(expected),
+        1e-3
+      );
     });
-
-    for (const poolName of CRYPTO_POOLS) {
-        it(`Claims CRV from ${poolName.toUpperCase()}`, async function () {
-            const pool = new curve.Pool(poolName);
-
-            const [crvBalanceBefore] = await curve.getBalances(['crv']) as string[];
-            const expected = await pool.gaugeClaimableTokens();
-
-            console.log(crvBalanceBefore, "+", expected, "=", Number(crvBalanceBefore) + Number(expected));
-            await pool.gaugeClaimTokens();
-
-            const [crvBalanceAfter] = await curve.getBalances(['crv']) as string[];
-            console.log(crvBalanceAfter);
-
-            assert.approximately(Number(crvBalanceAfter), Number(crvBalanceBefore) + Number(expected), 1e-3);
-        });
-    }
+  }
 });
